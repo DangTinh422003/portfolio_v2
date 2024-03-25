bbfe4db9d2c69b23fd3373aeaf40afa4b9c357f9
caodangtinh <caodangtinh04022003@gmail.com>
2024-03-25 23:02:39 +0700

    refactor: toggle theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,27 +1,21 @@
 "use client";
 import { useTheme } from "next-themes";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { FaSun } from "react-icons/fa6";
 import { IoMoon } from "react-icons/io5";
 
 const ToggleTheme = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
   const { setTheme } = useTheme();
 
-  const handleToggleTheme = () => {
-    if (isDarkTheme) {
-      setTheme("light");
-      setIsDarkTheme(false);
-    } else {
-      setTheme("dark");
-      setIsDarkTheme(true);
-    }
-  };
+  useLayoutEffect(() => {
+    isDarkTheme ? setTheme("dark") : setTheme("light");
+  }, [isDarkTheme, setTheme]);
 
   return (
     <button
       className="w-[50px] h-[50px] bg-[var(--primary)] rounded-full border-0 outline-none fixed z-50 top-[20px] right-[20px] flex-center text-white text-xl cursor-pointer shadow md:top-[50px] md:right-[50px]"
-      onClick={handleToggleTheme}
+      onClick={() => setIsDarkTheme(!isDarkTheme)}
     >
       {isDarkTheme ? <IoMoon /> : <FaSun />}
     </button>
