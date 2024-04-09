c19936d4e9a569796e02c237409497871e3f5e38
caodangtinh <caodangtinh04022003@gmail.com>
2024-04-10 02:34:07 +0700

    refactor: add loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { CgSpinner } from "react-icons/cg";
 
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <div className="flex-center w-screen h-screen">
+      <div className="animate-spin spin">
+        <CgSpinner className="text-4xl"/>
+      </div>
+    </div>
+  );
 };
 
 export default loading;
